919c7a7e76e5cdb8e4e7d836f41ea05d75416341
Deborah-Teta <154221965+Deborah-Teta@users.noreply.github.com>
2025-10-28 01:38:12 +0200

    tasks
    added sample tasks to easily help the user understand how the app functions

---
+++ ToDo-app/src/data/tasks.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+export default function Tasks() {
+    const tasks = [
+        
+                  {'id':1, 
+                   'name':"completed assignment", 
+                   'date':2025-9-15, 
+                   'priority':"high", 
+                   'status':"completed"
+
+                  },
+                  {'id':2, 
+                   'name':"Game project", 
+                   'date':"2025-11-20", 
+                   'priority':"medium", 
+                   'status':"active"
+
+                  },
+                  {'id':3, 
+                   'name':"Internship Application", 
+                   'date':"2025-10-12", 
+                   'priority':"medium", 
+                   'status':"active"
+
+                  },
+                  {'id':4, 
+                   'name':"shopping", 
+                   'date':"2025-10-18", 
+                   'priority':"low", 
+                   'status':"active"
+
+                  },
+                  {'id':5, 
+                   'name':"Certified test", 
+                   'date':"2025-09-22", 
+                   'priority':"high", 
+                   'status':"completed"
+
+                  }
+                
+    ];
+    return(tasks)
+}
